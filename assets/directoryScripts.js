549ca055d6126c8747ad65f78f8904b2fdc499de
danny <xdannyrobertsx@gmail.com>
2023-08-25 20:54:04 -0500

    display fix

--- assets/directoryScripts.js
+++ assets/directoryScripts.js
@@ -19,22 +19,20 @@ try {
 }
 
 const replaceMap = document.querySelector("#wc-directorySearchGoogleMap");
-
 if (replaceMap !== null) {
   const bannerContainer = document.querySelector("#bannerImage");
-  const bannerImage = bannerContainer.querySelector("img");
-
-  bannerImage.style.width = "100%";
-  bannerImage.style.maxWidth = "1000px";
-
+  const bannerImage = document.querySelector("#bannerImage img");
+  bannerImage.setAttribute("style", "width: 100%; max-width: 1000px;");
   replaceMap.innerHTML = bannerContainer.outerHTML;
-  replaceMap.classList.add("flex-container", "centered-container");
-
+  replaceMap.setAttribute(
+    "style",
+    "display: flex; justify-content: center; padding: 10px;"
+  );
   const newImage = document.querySelector("#memberDirectory #bannerImage");
-  newImage.classList.add("flex-container", "centered-container");
+  newImage.setAttribute(
+    "style",
+    "display: flex; justify-content: center; padding: 10px;"
+  );
 } else {
-  const bannerImage = document.querySelector("#bannerImage");
-  if (bannerImage) {
-    bannerImage.remove();
-  }
+  document.querySelector("#bannerImage").remove();
 }
